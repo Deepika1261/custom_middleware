e8e387efa5364f94b48573089f56ca04fb182b09
webP.deepika <deepikajamwal7@gmail.com>
2022-06-07 11:43:54 +0000

    middlewaare modified

--- custom_middleware/simple_middleware/middleware.py
+++ custom_middleware/simple_middleware/middleware.py
@@ -1,3 +1,4 @@
+from django.http.response import HttpResponse
 class SimpleMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
@@ -10,9 +11,9 @@ class SimpleMiddleware:
         #print(requests.get("http://127.0.0.1:8000/project", headers={"x-endeavour-token":"authenticated"}))
 
     def process_view(self, request, view_func, view_args, view_kwargs):
-        
-        if "authenticated" in request.headers['x-endeavour-token']:
-            print("present")
-            pass
+
+        if "authenticated" not in request.headers['x-endeavour-token']:
+            return HttpResponse(status=401)    
+        pass
 
         
